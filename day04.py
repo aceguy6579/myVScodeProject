daac691a92b3a72a34483077710ef986898f89dd
josh <aceguy6579@gmail.com>
2025-10-24 04:52:33 +0900

    Initial commit

---
+++ day04.py
@@ -0,0 +1,10 @@
+# input 함수로 표준 입력에서 데이터 읽어오기
+
+value = input('Enter an integer: ')
+
+print('value =', value)
+print()
+print('type(value) =', type(value))
+print()
+print('value + 10 =', int(value) + 10)
+print('Good bye!')
